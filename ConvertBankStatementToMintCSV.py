178f4af07c6463b20472fd740df8cf671f1b7b0b
Hannah <59471320+kaixin-hc@users.noreply.github.com>
2023-08-08 11:04:27 +0700

    Add python script

---
+++ ConvertBankStatementToMintCSV.py
@@ -0,0 +1,65 @@
+import pandas as pd;
+
+# Define functions to do conversion for different banks
+def convertScCsv(source, outputFilePath):
+    df=pd.read_csv(source,names=["Date", "Transaction", "Currency", "Deposit", "Withdrawal", "Running Balance", "SGD Equivalent Balance"])
+    # Cleanup: Drop the non-data columns on top
+    indToDropTo = df[df['Transaction']=='Transaction'].index.item()
+    df = df.iloc[(indToDropTo + 1):]
+    # Cleanup: Remove all the tabs from the date to clean the data, and change date format
+    df["Date"] = df["Date"].map(lambda x: x[-7:-4] + x[-10:-7] + x[-4:])
+    
+    # Format mint CSV: Date,Description,Original Description,Amount,Transaction Type,Category,Account Name,Labels,Notes
+    mintdf = pd.DataFrame({},columns=['Date','Description','Original Description','Amount','Transaction Type','Category','Account Name','Labels','Notes']);
+    mintdf["Date"] = df["Date"]
+    mintdf["Notes"] = df["Transaction"]
+    for index, row in df.iterrows():
+        if (pd.isna(row['Deposit'])):
+            mintdf["Amount"][index] = row["Withdrawal"]
+            mintdf["Transaction Type"][index] = "debit"
+        else:
+            mintdf["Amount"][index] = row["Deposit"]
+            mintdf["Transaction Type"][index] = "credit"
+    
+    mintdf.to_csv(outputFilePath, header=False, index=False)
+
+def convertDBSCsv(source, outputFilePath):
+    df=pd.read_csv("/Users/hannahchia/Downloads/DBSdata.csv",names=["Date", "Reference", "Debit Amount", "Credit Amount", "Transaction Ref1", "Transaction Ref2","Transaction Ref3"])
+    # Drop columns above the data
+    headerInd = df[df['Reference']=='Reference'].index.item()
+    df = df.iloc[(headerInd + 3):] # there are extra 2 rows below the header which are empty
+
+    # Reformat dates
+    df["Date"] = pd.to_datetime(df["Date"])
+    df["Date"] = df['Date'].dt.strftime('%m/%d/%Y')
+    
+    # Format mint CSV: Date,Description,Original Description,Amount,Transaction Type,Category,Account Name,Labels,Notes
+    mintdf = pd.DataFrame({},columns=['Date','Description','Original Description','Amount','Transaction Type','Category','Account Name','Labels','Notes']);
+    mintdf["Date"] = df["Date"]
+    df.update(df[["Transaction Ref1", "Transaction Ref2", "Transaction Ref3"]].fillna(''))
+    mintdf["Notes"] = df["Transaction Ref1"] + df["Transaction Ref2"] + df["Transaction Ref3"]
+    for index, row in df.iterrows():
+        if (row['Credit Amount'] == ' '):
+            mintdf["Amount"][index] = row["Debit Amount"]
+            mintdf["Transaction Type"][index] = "debit"
+        else:
+            mintdf["Amount"][index] = row["Credit Amount"]
+            mintdf["Transaction Type"][index] = "credit"
+    
+    mintdf.to_csv(outputFilePath, header=False, index=False)
+
+# Write main script
+source = input("Input the source file (expected to be a CSV of DBS or SC format): \n")
+outputFileName = input("Input the name of the file you want to create: \n")
+bank = input("Input SC or DBS: \n")
+while (bank != "SC" and bank != "DBS"):
+    bank = input("Input was not valid. Input SC or DBS. \n")
+
+if bank == "DBS":
+    convertDBSCsv(source, outputFileName)
+    print("Successfully created file from DBS csv at " + outputFileName)
+elif bank == "SC":
+    convertScCsv(source, outputFileName)
+    print("Successfully created file from SC csv at " + outputFileName)
+else:
+    print("Some error occurred. Maybe use the jupyter notebook instead")
